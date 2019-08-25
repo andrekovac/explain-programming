59bc8fb603f0d746b0bce66cff201f1778754138
André Kovac <me@andrekovac.com>
2019-08-25 16:34:16 +0200

    Set up google analytics and add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-81014342-3`,
       },
     },
     `gatsby-plugin-feed`,
