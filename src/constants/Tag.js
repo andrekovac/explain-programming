d7229d711bb8e32f68a8f220e4eb6f6a1e993b82
André Kovac <me@andrekovac.com>
2019-08-25 03:19:07 +0200

    Add possible tags in constants file

---
+++ src/constants/Tag.js
@@ -0,0 +1,20 @@
+/**
+ * All possible tags
+ */
+
+// THEORY
+export const CONCEPTS = 'concepts'
+export const DEFINITIONS = 'concepts'
+export const THEORY = 'theory'
+
+// LANGUAGES
+export const JAVASCRIPT = 'javascript'
+
+// FRAMEWORKS
+export const REACT = 'react'
+export const REACT_NATIVE = 'react-native'
+
+// MISC
+export const ANIMATIONS = 'animations'
+export const INTERNALS = 'internals'
+export const PERFORMANCE = 'performance'
