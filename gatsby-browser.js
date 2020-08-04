8de7a500acb478a81bd09279bb86bbb382de51e5
André Kovac <me@andrekovac.com>
2020-08-04 11:23:34 +0200

    Add Chakra.js UI dependency

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,17 @@
+import React from 'react';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import ChakraTheme from './src/style/chakraTheme';
+
 // custom typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
+import 'typeface-montserrat';
+import 'typeface-merriweather';
 
 // Trigger an immediate page refresh when an update is found
 export const onServiceWorkerUpdateReady = () => window.location.reload();
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={ChakraTheme}>
+    <CSSReset />
+    {element}
+  </ThemeProvider>
+);
