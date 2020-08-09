0d48cb1cbc5df9cc80baac70c99bddd65bf6cf03
André Kovac <me@andrekovac.com>
2020-08-10 00:53:48 +0200

    Fix profile links vertical alignment

--- src/components/profileLinks/styles.js
+++ src/components/profileLinks/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import theme from '../../style/theme';
 
 export const Link = styled.a`
-  font-size: 1.5em;
   color: ${theme.colors.white};
   background: none;
 
